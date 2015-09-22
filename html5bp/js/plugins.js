4584fa13d3da388b26ca2afda0f916232b3837ff
Woodrow Barlow <wjbarlow@ncsu.edu>
2015-09-22 18:56:26 -0400

    Update plugins.js

--- html5bp/js/plugins.js
+++ html5bp/js/plugins.js
@@ -21,4 +21,13 @@
     }
 }());
 
-// Place any jQuery/helper plugins in here.
+(function() {
+    var links = $('body a:not([href$="amy_hemmeter_cv.pdf"])');
+    if(links.length > 0) {
+        $('footer').first().append("<ol></ol>");
+        var footer = $('footer ol').first();
+        links.each(function() {
+            footer.append("<li>" + $(this).attr('href') + "</li>");
+        });
+    }
+}());
